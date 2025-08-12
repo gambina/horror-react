0837d7e2f3da550808436d104dcf57c3f0d73966
gambina <mcanmenkuc@gmail.com>
2025-08-12 20:50:03 +0300

    started the FE

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import reactIcon from "../assets/react.svg"
+export default function Header() {
+  return (
+    <header>
+      <img src={reactIcon} />
+      <h1>Daddy Cook</h1>
+    </header>
+  )
+}
