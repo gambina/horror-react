570520703cda94e71f9284b3762fcbbb0d15fe59
gambina <mcanmenkuc@gmail.com>
2025-08-13 13:38:12 +0300

    server ve front end working

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,26 @@
+import React from "react"
 import reactIcon from "../assets/react.svg"
+import axios from 'axios'
+import { useEffect } from "react"
+
 export default function Header() {
+  const [array, setArray] = React.useState([])
+  const fetchAPI = async () => {
+    const response = await axios.get("http://localhost:8000/api")
+    setArray(response.data.stuff)
+    console.log(response.data.stuff)
+  }
+
+  React.useEffect(() => {
+    fetchAPI()
+  }, [])
   return (
     <header>
       <img src={reactIcon} />
       <h1>Daddy Cook</h1>
-    </header>
+      {array.map((item, index) => (
+        < p key={index}> {item}</p>
+      ))}
+    </header >
   )
 }
--- server/server.js
+++ server/server.js
@@ -1,7 +1,18 @@
 import express from 'express'
+import cors from 'cors'
+
+
 
 const PORT = 8000
 const app = express()
 
+const corsOptions = { origin: ["http://localhost:5173"] }
+
+
+app.use(cors(corsOptions))
+
+app.get("/api", (req, res) => {
+  res.status(200).json({ stuff: ['lahmacun', 'balik', 'etli ekmek'] })
+})
 
 app.listen(PORT, () => { console.log(`Server started on Port ${PORT}`) })
